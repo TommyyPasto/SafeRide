e63313021c5def7a4a1b62edb3d0815b302f8841
TommyyPasto <pastorelli.tommaso@studenti-ittfedifermi.edu.it>
2022-02-24 09:43:42 +0100

    Cambiati i nomi degli elementi delle pagine *Corretto*

--- app/src/main/java/com/example/applicazionesaferide/Menu.java
+++ app/src/main/java/com/example/applicazionesaferide/Menu.java
@@ -3,6 +3,7 @@ package com.example.applicazionesaferide;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        Button btnMod1 = findViewById(R.id.btnMod1);
+        Button btnMod2 = findViewById(R.id.imageViewMod2);
+        Button btnMod3 = findViewById(R.id.btnMod3);
+        Button btnMod4 = findViewById(R.id.btnMod4);
+        Button btnMod5 = findViewById(R.id.btnMod5);
+        Button btnMod6 = findViewById(R.id.btnMod6);
     }
 }
