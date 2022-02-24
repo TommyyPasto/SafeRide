900666d8312696bd6d52f5d23680265a910b2b73
TommyyPasto <pastorelli.tommaso@studenti-ittfedifermi.edu.it>
2022-02-24 09:38:13 +0100

    Cambiati i nomi degli elementi delle pagine

--- app/src/main/java/com/example/applicazionesaferide/MainActivity.java
+++ app/src/main/java/com/example/applicazionesaferide/MainActivity.java
@@ -3,12 +3,10 @@ package com.example.applicazionesaferide;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageButton ib1=(ImageButton)findViewById(R.id.imageButton);
+        ImageButton ib1=(ImageButton)findViewById(R.id.imageButtonSettings);
         ib1.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
