1f95786e6e2f23b0b1eb55829e365ecee99c2285
TommyyPasto <pastorelli.tommaso@studenti-ittfedifermi.edu.it>
2022-02-24 09:21:55 +0100

    Grafica modificata ecc..

--- app/src/main/java/com/example/applicazionesaferide/MainActivity.java
+++ app/src/main/java/com/example/applicazionesaferide/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.applicazionesaferide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton ib1=(ImageButton)findViewById(R.id.imageButton);
+        ib1.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Menu.class);
+                startActivity(i);
+
+            }
+        });
+
+        Button buttonAlarm=(Button)findViewById(R.id.buttonAllarme);
+        buttonAlarm.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Allarme_Maps.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
